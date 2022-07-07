5edc6f1b8d33e5d274cf9b2894f9a7afaa89d6a5
jekepwnz <hardcoreokje@gmail.com>
2022-07-07 22:30:20 +0300

    add index.html with js script

--- JS/lesson_1.js
+++ JS/lesson_1.js
@@ -38,25 +38,18 @@ let age_2 = 18
 let age_3 = 60
 
 const checkAge = function(age) {
-    age = Number(age)
-if(typeof age == 'number'){
+if(age && !isNaN(age)){
     if (age < age_2){
-      console.log("You don't have access cause your age is " + age)
+      alert("You don't have access cause your age is " + age)
     } else if(age >= age_2 && age <= age_3){
-        console.log("Welcome!")
+        alert("Welcome!")
     } else if(age > age_3){
-       console.log("Ceep calm and watch Culture channel")
+        alert("Ceep calm and watch Culture channel")
     }else{
-     console.log("Technical work.")
+        alert("Technical work.")
     }
 }else{
-    console.log("Wrong input")
+    alert("Wrong input")
 }
 }
-
-checkAge(17)
-checkAge(18)
-checkAge(61)
-checkAge("aaa")
-checkAge("20")
-checkAge("2a")
+checkAge(prompt("Enter your age"))
